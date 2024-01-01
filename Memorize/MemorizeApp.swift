76faa89a9bcfbf21f6edd68641b412dfd14efc62
pilot-max <max.udaskin@gmail.com>
2024-01-01 17:40:09 -0500

    Initial Commit CardView isFaceUp

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Max Udaskin on 2024-01-01.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
