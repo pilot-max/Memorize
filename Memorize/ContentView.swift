507478253c681acbaa7e61bc1189afd7edde3a59
pilot-max <max.udaskin@gmail.com>
2024-01-02 12:46:34 -0500

    Put emojis into an array

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["💩", "😂", "🤷🏻‍♂️", "😱"]
+    
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,6 +22,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
     
     var body: some View {
@@ -29,7 +31,7 @@ struct CardView: View {
             if isFaceUp {
                 card.foregroundColor(.white)
                 card.strokeBorder(lineWidth: 2)
-                    Text("💩").font(.largeTitle)
+                    Text(content).font(.largeTitle)
             } else {
                 card
             }
